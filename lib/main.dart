268e82ab353fa93611a4d72c2e62eff2afde459f
koyasaeki <koyasaeki@gmail.com>
2024-01-27 12:04:29 +0900

    update: アイコンを表示する

--- lib/main.dart
+++ lib/main.dart
@@ -56,6 +56,26 @@ class _MyHomePageState extends State<MyHomePage> {
               print("ボタンが押されたよ。");
             },
             child: const Text('テキストボタン'),
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Icon(
+                Icons.favorite,
+                color: Colors.pink,
+                size: 24.0,
+              ),
+              Icon(
+                Icons.audiotrack,
+                color: Colors.green,
+                size: 30.0,
+              ),
+              Icon(
+                Icons.beach_access,
+                color: Colors.blue,
+                size: 36.0,
+              ),
+            ],
           )
         ],
       ),
