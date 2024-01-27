2d9585082e7508cd74ebbeaa35383c4e4703905b
koyasaeki <koyasaeki@gmail.com>
2024-01-27 12:11:28 +0900

    update: FloatingButton のアイコンを変える

--- lib/main.dart
+++ lib/main.dart
@@ -32,14 +32,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -84,9 +76,10 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: () {
+          print("押したね？");
+        },
+        child: const Icon(Icons.timer),
       ),
     );
   }
