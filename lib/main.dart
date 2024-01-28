57705aea69a2272bb139e4980008f3b8196c1cf7
koyasaeki <koyasaeki@gmail.com>
2024-01-29 00:30:59 +0900

    update: tween を使ってアニメーションを表現する

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,10 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   late AnimationController _animationController;
+  late Animation<double> _animationDouble;
+  late Animation<Color?> _animationColor;
+  final Tween<double> _tweenDouble = Tween(begin: 0.0, end: 200.0);
+  final ColorTween _tweenColor = ColorTween(begin: Colors.green, end: Colors.blue);
 
   _forward() async {
     setState(() {
@@ -55,10 +59,21 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   @override
   void initState() {
     super.initState();
-    _animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 3),
-    );
+    _animationController = AnimationController(vsync: this, duration: const Duration(seconds: 3));
+
+    // Tween と AnimationController から Animation (サイズ) を作成する。
+    _animationDouble = _tweenDouble.animate(_animationController);
+    // 変更を反映させるためにやっているだけ。
+    _animationDouble.addListener(() {
+      setState(() {});
+    });
+
+    // Tween と AnimationController から Animation (色) を作成する。
+    _animationColor = _tweenColor.animate(_animationController);
+    // 変更を反映させるためにやっているだけ。
+    _animationColor.addListener(() {
+      setState(() {});
+    });
   }
 
   @override
@@ -77,10 +92,19 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Text('AnimationController: ${_animationController.value}'),
+            Text('AnimationDouble: ${_animationDouble.value}'),
+            Text('AnimationColor: ${_animationColor.value}'),
             SizeTransition(
               sizeFactor: _animationController,
-              child: const Center(
-                child: FlutterLogo(size: 200.0),
+              child: Center(
+                child: SizedBox(
+                  width: _animationDouble.value,
+                  height: _animationDouble.value,
+                  child: Container(
+                    color: _animationColor.value,
+                  ),
+                ),
               ),
             ),
           ],
